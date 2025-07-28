ab8c521548ae764d676164f08d4d0c14d7747206
JElliott84 <164680937+JElliott84@users.noreply.github.com>
2025-07-27 22:33:41 -0400

    Update Mod 7 views.py

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
+def post_new(request):
+    [...]
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 # Create your views here.
+
+@login_required
+def post_new(request):
+    [...]
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
